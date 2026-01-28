affe888788a37ec632cd0c1778d12ad0b1b3a3cd
sainakka09 <sai.nakka09@gmail.com>
2026-01-28 11:21:11 +0530

    commit

--- variables.js
+++ variables.js
@@ -1,2 +1,6 @@
 var firstName = "Octane";
-let firstname = "Octane19";
+console.log(firstName);
+firstName = "botwa";
+let firstname = "Octane19"; //camel case notation
+console.log(firstName);
+console.log(firstname);
