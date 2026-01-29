9f4f779140e498368b9e27f3e2f63465062f3aee
sainakka09 <sai.nakka09@gmail.com>
2026-01-29 08:56:12 +0530

    commit

---
+++ datatypes.js
@@ -0,0 +1,31 @@
+// there are two datatypes they are primitive and reference datatypes
+// string datatype
+let a = "bot1";
+let b = "bot2";
+let c = `java`;
+
+// number datatype
+let number_bots = 12;
+let pi = 3.14;
+
+// big int literal
+let verylargenumber = 2332232323328n;
+
+// boolean
+let aa = true;
+
+// undefined datatype
+//This means the value is not assigned to a variable
+let fav;
+console.log(fav);
+
+// Null datatype
+let favbot = null;
+
+// symbol datatype Used to create unique identifiers.
+const uniqueKey = Symbol();
+
+let course = {
+  name: "java",
+  time: 3,
+};
--- variables.js
+++ variables.js
@@ -4,3 +4,8 @@ firstName = "botwa";
 let firstname = "Octane19"; //camel case notation
 console.log(firstName);
 console.log(firstname);
+// let keyword is used when the values are needed to be changed
+// const keyword is used when the values are not needed to be changed
+const botwa = "12.23.445";
+// botwa = "23.43.44"; TypeError: Assignment to constant variable.
+console.log(botwa);
