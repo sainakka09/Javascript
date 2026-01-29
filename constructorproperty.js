9d274b7b91c22c817b438ae0b1293091ddaf320d
sainakka09 <sai.nakka09@gmail.com>
2026-01-29 16:30:28 +0530

    js

---
+++ constructorproperty.js
@@ -0,0 +1,9 @@
+const sai = {
+  name: "sai",
+  age: 20,
+};
+let newobj1 = {};
+let newobj2 = new Object();
+new String();
+new Boolean();
+new Number();
---
+++ func_are_objects.js
@@ -0,0 +1,6 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+const n = add;
+console.log(n(2, 3));
+console.log(add.length);
